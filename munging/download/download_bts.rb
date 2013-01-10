5aa986d870ab32fedfa96ffbc8c156c5f20f7148
Dieterich Lawson <dieterich.lawson@gmail.com>
2013-01-09 22:14:45 -0600

    More changes to bts download script

--- munging/download/download_bts.rb
+++ munging/download/download_bts.rb
@@ -10,27 +10,64 @@ Settings.define :years, flag: 'y', description: 'Years to retrieve', type: Array
 Settings.define :months, flag: 'm', description: "Months to retrieve", type: Array, default: MONTHS
 Settings.define :out_dir, flag: 'o', description: "Directory to put files in", default: '/data/origin/www.transtats.bts.gov'
 Settings.define :local, flag: 'l', description: "Put downloaded files on local filesystem", default: false
+Settings.define :tmp_dir, description: "Place to temporarily put the zip archives before extraction", default: "#{Dir.pwd}/.tmp"
 Settings.resolve!
 
 def get_post_params(year,month,month_num)
   "UserTableName=On_Time_Performance&DBShortName=On_Time&RawDataTable=T_ONTIME&sqlstr=+SELECT+YEAR%2CMONTH%2CDAY_OF_MONTH%2CDAY_OF_WEEK%2CUNIQUE_CARRIER%2CTAIL_NUM%2CFL_NUM%2CORIGIN_AIRPORT_ID%2CORIGIN_STATE_FIPS%2CDEST_AIRPORT_ID%2CCRS_DEP_TIME%2CDEP_DELAY_NEW%2CDEP_DEL15%2CDEP_DELAY_GROUP%2CCRS_ARR_TIME%2CARR_DELAY%2CCANCELLED%2CDIVERTED%2CCRS_ELAPSED_TIME%2CDISTANCE+FROM++T_ONTIME+WHERE+Month+%3D#{month_num}+AND+YEAR%3D#{year}&varlist=YEAR%2CMONTH%2CDAY_OF_MONTH%2CDAY_OF_WEEK%2CUNIQUE_CARRIER%2CTAIL_NUM%2CFL_NUM%2CORIGIN_AIRPORT_ID%2CORIGIN_STATE_FIPS%2CDEST_AIRPORT_ID%2CCRS_DEP_TIME%2CDEP_DELAY_NEW%2CDEP_DEL15%2CDEP_DELAY_GROUP%2CCRS_ARR_TIME%2CARR_DELAY%2CCANCELLED%2CDIVERTED%2CCRS_ELAPSED_TIME%2CDISTANCE&grouplist=&suml=&sumRegion=&filter1=title%3D&filter2=title%3D&geo=All%A0&time=#{month}&timename=Month&GEOGRAPHY=All&XYEAR=#{year}&FREQUENCY=1&VarName=YEAR&VarDesc=Year&VarType=Num&VarDesc=Quarter&VarType=Num&VarName=MONTH&VarDesc=Month&VarType=Num&VarName=DAY_OF_MONTH&VarDesc=DayofMonth&VarType=Num&VarName=DAY_OF_WEEK&VarDesc=DayOfWeek&VarType=Num&VarDesc=FlightDate&VarType=Char&VarName=UNIQUE_CARRIER&VarDesc=UniqueCarrier&VarType=Char&VarDesc=AirlineID&VarType=Num&VarDesc=Carrier&VarType=Char&VarName=TAIL_NUM&VarDesc=TailNum&VarType=Char&VarName=FL_NUM&VarDesc=FlightNum&VarType=Char&VarName=ORIGIN_AIRPORT_ID&VarDesc=OriginAirportID&VarType=Num&VarDesc=OriginAirportSeqID&VarType=Num&VarDesc=OriginCityMarketID&VarType=Num&VarDesc=Origin&VarType=Char&VarDesc=OriginCityName&VarType=Char&VarDesc=OriginState&VarType=Char&VarName=ORIGIN_STATE_FIPS&VarDesc=OriginStateFips&VarType=Char&VarDesc=OriginStateName&VarType=Char&VarDesc=OriginWac&VarType=Num&VarName=DEST_AIRPORT_ID&VarDesc=DestAirportID&VarType=Num&VarDesc=DestAirportSeqID&VarType=Num&VarDesc=DestCityMarketID&VarType=Num&VarDesc=Dest&VarType=Char&VarDesc=DestCityName&VarType=Char&VarDesc=DestState&VarType=Char&VarDesc=DestStateFips&VarType=Char&VarDesc=DestStateName&VarType=Char&VarDesc=DestWac&VarType=Num&VarName=CRS_DEP_TIME&VarDesc=CRSDepTime&VarType=Char&VarDesc=DepTime&VarType=Char&VarDesc=DepDelay&VarType=Num&VarName=DEP_DELAY_NEW&VarDesc=DepDelayMinutes&VarType=Num&VarName=DEP_DEL15&VarDesc=DepDel15&VarType=Num&VarName=DEP_DELAY_GROUP&VarDesc=DepartureDelayGroups&VarType=Num&VarDesc=DepTimeBlk&VarType=Char&VarDesc=TaxiOut&VarType=Num&VarDesc=WheelsOff&VarType=Char&VarDesc=WheelsOn&VarType=Char&VarDesc=TaxiIn&VarType=Num&VarName=CRS_ARR_TIME&VarDesc=CRSArrTime&VarType=Char&VarDesc=ArrTime&VarType=Char&VarName=ARR_DELAY&VarDesc=ArrDelay&VarType=Num&VarDesc=ArrDelayMinutes&VarType=Num&VarDesc=ArrDel15&VarType=Num&VarDesc=ArrivalDelayGroups&VarType=Num&VarDesc=ArrTimeBlk&VarType=Char&VarName=CANCELLED&VarDesc=Cancelled&VarType=Num&VarDesc=CancellationCode&VarType=Char&VarName=DIVERTED&VarDesc=Diverted&VarType=Num&VarName=CRS_ELAPSED_TIME&VarDesc=CRSElapsedTime&VarType=Num&VarDesc=ActualElapsedTime&VarType=Num&VarDesc=AirTime&VarType=Num&VarDesc=Flights&VarType=Num&VarName=DISTANCE&VarDesc=Distance&VarType=Num&VarDesc=DistanceGroup&VarType=Num&VarDesc=CarrierDelay&VarType=Num&VarDesc=WeatherDelay&VarType=Num&VarDesc=NASDelay&VarType=Num&VarDesc=SecurityDelay&VarType=Num&VarDesc=LateAircraftDelay&VarType=Num&VarDesc=FirstDepTime&VarType=Char&VarDesc=TotalAddGTime&VarType=Num&VarDesc=LongestAddGTime&VarType=Num&VarDesc=DivAirportLandings&VarType=Num&VarDesc=DivReachedDest&VarType=Num&VarDesc=DivActualElapsedTime&VarType=Num&VarDesc=DivArrDelay&VarType=Num&VarDesc=DivDistance&VarType=Num&VarDesc=Div1Airport&VarType=Char&VarDesc=Div1AirportID&VarType=Num&VarDesc=Div1AirportSeqID&VarType=Num&VarDesc=Div1WheelsOn&VarType=Char&VarDesc=Div1TotalGTime&VarType=Num&VarDesc=Div1LongestGTime&VarType=Num&VarDesc=Div1WheelsOff&VarType=Char&VarDesc=Div1TailNum&VarType=Char&VarDesc=Div2Airport&VarType=Char&VarDesc=Div2AirportID&VarType=Num&VarDesc=Div2AirportSeqID&VarType=Num&VarDesc=Div2WheelsOn&VarType=Char&VarDesc=Div2TotalGTime&VarType=Num&VarDesc=Div2LongestGTime&VarType=Num&VarDesc=Div2WheelsOff&VarType=Char&VarDesc=Div2TailNum&VarType=Char&VarDesc=Div3Airport&VarType=Char&VarDesc=Div3AirportID&VarType=Num&VarDesc=Div3AirportSeqID&VarType=Num&VarDesc=Div3WheelsOn&VarType=Char&VarDesc=Div3TotalGTime&VarType=Num&VarDesc=Div3LongestGTime&VarType=Num&VarDesc=Div3WheelsOff&VarType=Char&VarDesc=Div3TailNum&VarType=Char&VarDesc=Div4Airport&VarType=Char&VarDesc=Div4AirportID&VarType=Num&VarDesc=Div4AirportSeqID&VarType=Num&VarDesc=Div4WheelsOn&VarType=Char&VarDesc=Div4TotalGTime&VarType=Num&VarDesc=Div4LongestGTime&VarType=Num&VarDesc=Div4WheelsOff&VarType=Char&VarDesc=Div4TailNum&VarType=Char&VarDesc=Div5Airport&VarType=Char&VarDesc=Div5AirportID&VarType=Num&VarDesc=Div5AirportSeqID&VarType=Num&VarDesc=Div5WheelsOn&VarType=Char&VarDesc=Div5TotalGTime&VarType=Num&VarDesc=Div5LongestGTime&VarType=Num&VarDesc=Div5WheelsOff&VarType=Char&VarDesc=Div5TailNum&VarType=Char"
 end
 
-def download(year,month,out_dir)
-  curl = "curl -L -d \"#{get_post_params(year,month,MONTHS.index(month)+1)}\" 'http://www.transtats.bts.gov/DownLoad_Table.asp?Table_ID=236&Has_Group=3&Is_Zipped=0'"
-  out_file = "#{out_dir}/#{year}-#{month}.csv"
-  cmd = "#{curl} | unzip -p"
-  if Settings.local
-    cmd += " > #{out_file}"
+def dir_exists? (dir,local=true)
+  if local
+    File.directory? dir 
   else
-    cmd += " | hadoop fs -put - #{out_file}"
+    `hadoop fs -test -e #{dir}`
+    $? == 0
   end
-  puts "#{Settings.local ? "Downloading" : "Uploading"} #{month} #{year} to #{out_file}"
-  `#{cmd}`
 end
 
-Settings.years.each do |year|
-  Settings.months.each_with_index do |month,index|
-    download(year,month,Settings.out_dir)
+def mkdir(dir,local=true)
+  if local
+    FileUtils.mkdir_p dir  
+  else
+    `hadoop fs -mkdir #{dir}`
   end
 end
+
+def mkdir_unless_exists(dir,local=true)
+  mkdir(dir,local) unless dir_exists?(dir,local)
+end
+
+def download(year,month,out_dir)
+  curl = "curl -L -d \"#{get_post_params(year,month,MONTHS.index(month)+1)}\" 'http://www.transtats.bts.gov/DownLoad_Table.asp?Table_ID=236&Has_Group=3&Is_Zipped=0'"
+  out_file = "#{out_dir}/#{year}-#{month}.zip"
+  puts "Downloading #{month} #{year} to #{out_file}"
+  `#{curl} > #{out_file}`
+end
+
+def unzip(year,month,orig_dir,dest_dir)
+  puts "Unzipping and putting #{month} #{year} into place"
+  zip_archive = "#{orig_dir}/#{year}-#{month}.zip"
+  if Settings.local
+    `unzip -p #{zip_archive} > #{dest_dir}/#{month}.zip`
+  else
+    `unzip -p #{zip_archive} | hadoop fs -put #{dest_dir}/#{month}.zip`
+  end
+  `rm #{zip_archive}`
+end
+
+if Settings.local and Settings.out_dir = '/data/origin/www.transtats.bts.gov'
+  Settings.out_dir = Dir.pwd
+end
+
+mkdir_unless_exists Settings.tmp_dir, Settings.local
+
+Settings.years.each do |year|
+  out_dir = Settings.out_dir + "/#{year}"
+  mkdir_unless_exists(out_dir, Settings.local)
+  Settings.months.each_with_index do |month,index|
+    download(year,month,Settings.tmp_dir)
+    unzip(year,month,Settings.tmp_dir,out_dir) 
+  end
+end
+`rm -r #{Settings.tmp_dir}`
